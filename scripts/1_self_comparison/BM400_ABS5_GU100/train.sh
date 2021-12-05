df7326f1cf78dab83cc1e2f423ff56c36a3dffb8
HAXRD <haxrd@stu.xmu.edu.cn>
2021-12-05 10:36:58 +0800

    [pretrain | train] update scripts to add backup

--- scripts/1_self_comparison/BM400_ABS5_GU100/train.sh
+++ scripts/1_self_comparison/BM400_ABS5_GU100/train.sh
@@ -11,6 +11,10 @@ scenario=pattern
 name_addon=1_self_comparison
 collect_strategy=default
 
+# bak files
+cp -r results_${name_addon} results_${name_addon}_bak
+echo "back up original files"
+
 CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} \
 python pretrain_train.py \
 --scenario ${scenario} --name_addon ${name_addon} \

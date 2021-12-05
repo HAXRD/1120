a60f68f7103ef3dc97adeea313d44c51221e10c5
HAXRD <haxrd@stu.xmu.edu.cn>
2021-12-05 09:05:57 +0800

    [pretrain | train] scripts update

--- scripts/1_self_comparison/BM100_ABS5_GU100/train.sh
+++ scripts/1_self_comparison/BM100_ABS5_GU100/train.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CUDA_VISIBLE_DEVICES=0
 seed=0
 
 n_BM=100
@@ -10,6 +11,7 @@ scenario=pattern
 name_addon=1_self_comparison
 collect_strategy=default
 
+CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} \
 python pretrain_train.py \
 --scenario ${scenario} --name_addon ${name_addon} \
 --seed ${seed} \

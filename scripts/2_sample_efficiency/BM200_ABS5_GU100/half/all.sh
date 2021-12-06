feed5476210fe3da665ed4ae46d17cd1cd22904a
HAXRD <haxrd@stu.xmu.edu.cn>
2021-12-06 08:21:33 +0800

    [scripts] upload all.sh for 2_sample_efficiency

--- scripts/2_sample_efficiency/BM200_ABS5_GU100/half/all.sh
+++ scripts/2_sample_efficiency/BM200_ABS5_GU100/half/all.sh
@@ -3,7 +3,7 @@
 # run collect_s${i}.sh
 for i in 1 2 3 4
 do
-    ./scripts/2_sample_efficiency/BM200_ABS5_GU100/collect_s${i}.sh &
+    ./scripts/2_sample_efficiency/BM200_ABS5_GU100/half/collect_s${i}.sh &
     pids[${i}]=$!
 done
 
@@ -14,10 +14,10 @@ do
 done
 
 # run train.sh
-./scripts/2_sample_efficiency/BM200_ABS5_GU100/train.sh
+./scripts/2_sample_efficiency/BM200_ABS5_GU100/half/train.sh
 
 # run test.sh
-./scripts/2_sample_efficiency/BM200_ABS5_GU100/test.sh
+./scripts/2_sample_efficiency/BM200_ABS5_GU100/half/test.sh
 
 # run eval.sh
-./scripts/2_sample_efficiency/BM200_ABS5_GU100/eval.sh
+./scripts/2_sample_efficiency/BM200_ABS5_GU100/half/eval.sh

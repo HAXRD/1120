9d73e022eb08f51af3b3d3a71539cc671995ac36
HAXRD <haxrd@stu.xmu.edu.cn>
2021-12-08 19:19:10 +0800

    [4_justification] update justification part to select most suitable n_step_explore

---
+++ scripts/4_justification/BM200_n_explore_20/me.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CUDA_VISIBLE_DEVICES=2
+seed=0
+
+n_BM=200
+
+scenario=pattern
+name_addon=4_justification_n_explore_20
+collect_strategy=default
+
+eval_emulator_fpath=results_1_self_comparison/BM200_ABS5_GU100_default/emulator_ckpts/best_emulator.pt
+num_eval_episodes=100
+num_episodes_per_trial=1
+n_step_explore=20
+n_step_serve=80
+
+method=map-elites
+
+
+CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} \
+python eval_4_justification.py \
+--scenario ${scenario} --name_addon ${name_addon} \
+--collect_strategy ${collect_strategy} \
+--seed ${seed} \
+--eval_emulator_fpath ${eval_emulator_fpath} \
+--num_eval_episodes ${num_eval_episodes} \
+--num_episodes_per_trial ${num_episodes_per_trial} \
+--n_BM ${n_BM} \
+--method ${method} \
+--n_step_explore ${n_step_explore} --n_step_serve ${n_step_serve}

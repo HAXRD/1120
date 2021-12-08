7e45daadf95a639a0489cde77b48751c225b5ed9
HAXRD <haxrd@stu.xmu.edu.cn>
2021-12-08 12:00:30 +0800

    [3_adaptive_to_variable_entities] update code and scripts

--- scripts/3_adaptive_to_variable_entities/BM200/vGU.sh
+++ scripts/3_adaptive_to_variable_entities/BM200/vGU.sh
@@ -6,16 +6,16 @@ seed=0
 n_BM=200
 
 scenario=pattern
-name_addon=3_adaptive_to_variable_entities
+name_addon=3_adaptive_to_variable_entities_var_GU
 collect_strategy=default
 
-eval_emulator_fpath=results_1_self_comparison/BM200_ABS5_GU100_default/emulator_ckpts/best_emulator.pt
+eval_emulator_fpath=results_3_adaptive_to_variable_entities/BM200_ABS5_GU200_default/emulator_ckpts/best_emulator.pt
 
 
 ############### emulator accuracy ################
 num_eval_episodes=2_000
 # run emulator accuracy test
-for n in 75 125
+for n in 75 100 125
 do
     CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} \
     python eval_emulator.py \
@@ -36,7 +36,7 @@ done
 unset pids
 
 # run emulator accuracy test
-for n in 150 200
+for n in 150 175 200
 do
     CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} \
     python eval_emulator.py \
@@ -58,7 +58,7 @@ unset pids
 
 ############### method performance ################
 num_eval_episodes=100
-for n in 75 125 150 200
+for n in 75 100 125 150 175 200
 do
     for method in naive-kmeans mutation-kmeans map-elites
     do
